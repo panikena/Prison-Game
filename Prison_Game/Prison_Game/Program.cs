d485e34a94d3bf3fa66e0a3f6a34545f2e79ac95
panikena <adenisenko@ymail.com>
2016-05-12 23:38:28 +0300

    Initial commit

---
+++ Prison_Game/Prison_Game/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prison_Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+      
+            Console.WriteLine("Press enter to start game!");
+            string action;
+        }
+    }
+}
